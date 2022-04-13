59a96db34cc38ae724dffb310d6e57c222a44a28
Hansem Ro <hansemro@outlook.com>
2022-04-12 18:26:34 -0700

    demo/Main.gd: create function to draw from PoolByteArray

--- demo/Main.gd
+++ demo/Main.gd
@@ -8,17 +8,9 @@ func _ready():
 	print(gdclip.has_image())
 	if gdclip.has_image():
 		var image_byte_array = gdclip.get_image_as_pbarray()
-		var test_sprite = Sprite.new()
-		var texture = ImageTexture.new()
-		var image = Image.new()
 		var size = gdclip.get_image_size()
 		print(size)
-		image.create_from_data(size[0], size[1], false, Image.FORMAT_RGBA8, image_byte_array)
-		print(image.get_size())
-		texture.create_from_image(image)
-		test_sprite.set_texture(texture)
-		test_sprite.position = get_viewport().get_visible_rect().size/2
-		add_child(test_sprite)
+		draw_pbarray(image_byte_array, size[0], size[1])
 	test_gdclip_not_null()
 	print("test_gdclip_not_null Passed")
 	test_clear_1()
@@ -57,6 +49,17 @@ func _ready():
 	print("test_set_image_as_pbarray_1 Passed")
 	test_set_image_from_pbarray_2()
 	print("test_set_image_as_pbarray_2 Passed")
+
+func draw_pbarray(pbarray: PoolByteArray, width: int, height: int):
+	var test_sprite = Sprite.new()
+	var texture = ImageTexture.new()
+	var image = Image.new()
+	image.create_from_data(width, height, false, Image.FORMAT_RGBA8, pbarray)
+	print(image.get_size())
+	texture.create_from_image(image)
+	test_sprite.set_texture(texture)
+	test_sprite.position = get_viewport().get_visible_rect().size/2
+	add_child(test_sprite)
 
 # TEST functions
 
